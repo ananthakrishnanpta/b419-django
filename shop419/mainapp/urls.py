445645c9f53439b5caaa0dccf6719d2737a71f3a
ananthakrishnanpta <ananthakrishnanpta@gmail.com>
2025-02-17 16:10:15 +0530

    added crud operations

--- shop419/mainapp/urls.py
+++ shop419/mainapp/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 
-from .views import homeView, aboutView # importing the views from the mainapp to access the functions
+from .views import homeView, aboutView, AddProduct, ProductDetails, UpdateProduct, DeleteProduct
+# importing the views from the mainapp to access the functions
 
 urlpatterns = [
     path("", homeView, name='homepage'),
-    path("about", aboutView, name="aboutpage")
+    path("about", aboutView, name="aboutpage"),
+
+    path('products/add',            AddProduct.as_view(),     name='add_prod'),       # C
+    path('products/<int:pk>',       ProductDetails.as_view(), name='prod_details'),   # R
+    path('products/edit/<int:pk>',  UpdateProduct.as_view(),  name = 'edit_prod'),    # U 
+    path('products/del/<int:pk>',   DeleteProduct.as_view(),  name = 'del_prod')      # D
 ]
+
 
--- shop419/mainapp/views.py
+++ shop419/mainapp/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from .models import Product
 
+# importing url resolution tools
+from django.urls import reverse, reverse_lazy
+
 # to help load the template file
 from django.template import loader
 
 # to help return an Http response to the user for any given request
 from django.http import HttpResponse
+
+# importing the generic class based views for CRUD operations
+
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
 # Create your views here.
 
@@ -29,3 +36,38 @@ def aboutView(request):
     }
     template = loader.get_template('about.html')
     return HttpResponse(template.render(context, request))
+
+
+
+# Views for adding, editing and deleting products
+ 
+# C R U D
+# Create
+class AddProduct(CreateView):
+    model = Product
+    fields = ['name', 'price','desc','pic','stock']
+    template_name = 'addProduct.html'
+    success_url = reverse_lazy('homepage') 
+
+# Read -> show details of each product
+class ProductDetails(DetailView):
+    model = Product
+    template_name = 'prod_details.html'
+    context_object_name = 'prod'
+
+# Update ->
+
+class UpdateProduct(UpdateView):
+    model = Product
+    fields = '__all__'
+    template_name = 'editProduct.html'
+
+    # overriding a method to produce dynamic redirection according to product id.
+    def get_success_url(self):
+        return reverse('prod_details', kwargs = {'pk' : self.object.pk})
+
+# Delete 
+class DeleteProduct(DeleteView):
+    model = Product
+    template_name = 'delProduct.html'
+    success_url = reverse_lazy('homepage')
