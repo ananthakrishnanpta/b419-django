f57bdb94db42b9b0f226aec630d926661fc8ecf4
ananthakrishnanpta <ananthakrishnanpta@gmailcom>
2025-02-18 15:44:09 +0530

    added search view

--- shop419/mainapp/views.py
+++ shop419/mainapp/views.py
@@ -78,6 +78,7 @@ class DeleteProduct(DeleteView):
 def searchView(request):
     query = request.GET.get('search_text') 
     # fetch the query text from GET request 
+    # This runs 'SELECT * FROM product WHERE name like '%<query>%';
     
     results = Product.objects.filter(name__icontains = query) 
     # collect the product objects matching the name
