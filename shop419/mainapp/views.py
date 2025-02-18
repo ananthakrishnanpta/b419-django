ea20970769bf727f4a3ffdeb784e868d1d0261bc
ananthakrishnanpta <ananthakrishnanpta@gmailcom>
2025-02-18 15:45:25 +0530

    added search view

--- shop419/mainapp/views.py
+++ shop419/mainapp/views.py
@@ -78,10 +78,13 @@ class DeleteProduct(DeleteView):
 def searchView(request):
     query = request.GET.get('search_text') 
     # fetch the query text from GET request 
-    # This runs 'SELECT * FROM product WHERE name like '%<query>%';
+    
     
     results = Product.objects.filter(name__icontains = query) 
     # collect the product objects matching the name
+    # This runs 'SELECT * FROM product WHERE name like '%<query>%';
+    # icontains is case-insensitive
+    # contains can be used for case-sensitive
     
     context = {
         'items' : results,
