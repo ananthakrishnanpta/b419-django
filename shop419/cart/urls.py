dd2b18e3dbbdf39fbdb08406552eb680e8eecad9
ananthakrishnanpta <ananthakrishnanpta@gmailcom>
2025-02-27 15:38:45 +0530

    edits

--- shop419/cart/urls.py
+++ shop419/cart/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
-from .views import viewCart, addToCart,remFromCart
+from .views import viewCart, addToCart,remFromCart, addQuantity, remQuantity
 
 
 urlpatterns = [
     path('cart/', viewCart, name = 'view_cart'),
     path('cart/add/<int:product_id>', addToCart, name = 'add_to_cart'),
-    path('cart/rem/<int:cart_item_id>', remFromCart, name = 'rem_from_cart')
+    path('cart/rem/<int:cart_item_id>', remFromCart, name = 'rem_from_cart'),
+
+        # The following url patterns will be requested by the JS function
+    path('cart/add/<int:cart_item_id>/', addQuantity, name='add_quantity'),
+    path('cart/remove/<int:cart_item_id>/', remQuantity, name='rem_quantity'),
 ]
--- shop419/cart/views.py
+++ shop419/cart/views.py
@@ -2,7 +2,14 @@ from django.shortcuts import render, redirect
 from mainapp.models import Product
 from .models import CartItem
 
+# to redirect to login page when required.
 from django.contrib.auth.decorators import login_required
+
+
+# implementing AJAX to update cart item quantity without refresh
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+
 # Create your views here.
 
 # C - Creating cart items
@@ -41,5 +48,28 @@ def viewCart(request):
 def remFromCart(request, cart_item_id):
     this_cart_item = CartItem.objects.get(id = cart_item_id)
     this_cart_item.delete() # deletes the associated record as well as the cartitem obj in memory
-    
+
     return redirect('view_cart')
+
+
+
+@login_required
+def addQuantity(request, cart_item_id):
+    cart_item = get_object_or_404(CartItem, id=cart_item_id, user=request.user)
+    cart_item.quantity += 1
+    cart_item.save()
+    overall_total = sum(item.get_total_price() for item in CartItem.objects.filter(user=request.user))
+    return JsonResponse({'quantity': cart_item.quantity, 'total_price': cart_item.get_total_price(), 'overall_total': overall_total})
+
+@login_required
+def remQuantity(request, cart_item_id):
+    cart_item = get_object_or_404(CartItem, id=cart_item_id, user=request.user)
+    if cart_item.quantity > 1:
+        cart_item.quantity -= 1
+        cart_item.save()
+        overall_total = sum(item.get_total_price() for item in CartItem.objects.filter(user=request.user))
+        return JsonResponse({'quantity': cart_item.quantity, 'total_price': cart_item.get_total_price(), 'overall_total': overall_total})
+    else:
+        cart_item.delete()
+        overall_total = sum(item.get_total_price() for item in CartItem.objects.filter(user=request.user))
+        return JsonResponse({'quantity': 0, 'total_price': 0, 'overall_total': overall_total})
